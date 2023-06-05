19851a96ee960d5bf64dfff1854201b14d0a689c
Aza Bibo <biboaza37@gmail.com>
2023-06-05 23:21:06 +0900

    update reversi.c

--- reversi.c
+++ reversi.c
@@ -4,29 +4,26 @@
 #include "functions.h"
 
 int main(int argc, char ** argv) {
-
-    screen_start();
 	
     int conn_server_fd, conn_client_fd; 
 
     if(argc < 2) 
 	    error("Invalid number of arguments");
     if (strcmp(argv[1], "-client") == 0) { // 'X'
-	//TODO
-	if(argc != 4) error("Invalid number of arguments");
-	conn_client_fd = connect_ipaddr_port(argv[2], atoi(argv[3]));
-	chat_client(conn_client_fd);
+        //TODO
+        if(argc != 4) error("Invalid number of arguments");
+        conn_client_fd = connect_ipaddr_port(argv[2], atoi(argv[3]));
+        chat_client(conn_client_fd);
 	}
     else if(strcmp(argv[1], "-server") == 0) { // 'O' #STARTS
-   	//TODO
-	if(argc != 3) error("Invalid number of arguments");
-	conn_server_fd = listen_at_port(atoi(argv[2]));
-	chat_server(conn_server_fd);
+        //TODO
+        if(argc != 3) error("Invalid number of arguments");
+        conn_server_fd = listen_at_port(atoi(argv[2]));
+        chat_server(conn_server_fd);
     }  
     else {
-	error("Passing the wrong arguments");
+	    error("Passing the wrong arguments");
     }
-
 
     getch();
 
