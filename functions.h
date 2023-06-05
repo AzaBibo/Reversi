65caf5dd4a2eda91599d01cb376ece82686e8da4
AzaBibo <biboaza37@gmail.com>
2023-06-05 20:55:29 +0900

    main functions implemeneted

--- functions.h
+++ functions.h
@@ -5,10 +5,18 @@ void printBoard();
 
 void screen_start();
 
-void screen_end();
+void screen_end(int server, int client);
 
 void help();
 
 void error(char * message);
+
+int listen_at_port (int portnum);
+
+void chat_server (int conn_fd);
+
+int connect_ipaddr_port (const char * ip, int port);
+
+void chat_client (int conn_fd);
 
 #endif
--- reversi.c
+++ reversi.c
@@ -5,18 +5,23 @@
 int main(int argc, char ** argv) {
 
     screen_start();
+	
+    int conn_server_fd, conn_client_fd; 
 
     if(argc < 2) 
 	    error("Invalid number of arguments");
     if (strcmp(argv[1], "-client") == 0) { // 'X'
 	//TODO
-	if(argc != 3) error("Invalid number of arguments");
+	if(argc != 4) error("Invalid number of arguments");
     	printBoard();
+	conn_client_fd = connect_ipaddr_port(argv[2], atoi(argv[3]));
 	}
     else if(strcmp(argv[1], "-server") == 0) { // 'O' #STARTS
    	//TODO
-	if(argc != 4) error("Invalid number of arguments");
+	if(argc != 3) error("Invalid number of arguments");
     	printBoard();
+	conn_server_fd = listen_at_port(argv[2]);
+	chat_server(conn_server_fd);
     }  
     else {
 	error("Passing the wrong arguments");
@@ -25,7 +30,7 @@ int main(int argc, char ** argv) {
 
     getch();
 
-    screen_end();
+    screen_end(conn_server_fd, conn_client_fd);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
